1a276c9a36d61411605ef67386aa3a7117092f12
Illio, the Great <pwfcoding@gmail.com>
2019-08-13 12:34:05 -0500

    working code, now its time add next arrival and minutes away to firebase and then append the table

--- app.js
+++ app.js
@@ -44,16 +44,54 @@ $("#add-train").on("click", function (event) {
     });
 });
 
-dataRef.ref().on("child_added", function(childSnapshot) {
+dataRef.ref().on("child_added", function (childSnapshot) {
 
     console.log(childSnapshot.val().name);
     console.log(childSnapshot.val().destination);
     console.log(childSnapshot.val().firstTrainTime);
     console.log(childSnapshot.val().frequency);
 
+    var tFrequency = frequency;
+
+    // Time is 3:30 AM
+    var firstTime = firstTrainTime;
+
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
+
+    // Current Time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % tFrequency;
+    console.log(tRemainder);
+
+    // Minute Until Train
+    var tMinutesTillTrain = tFrequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+
+    dataRef.ref().push({
+
+
+    })
+
+
+
+    // code to create a row with saved information from database
     var newRowItem = $("<tr><td>" + childSnapshot.val().name + "</td><td>" + childSnapshot.val().destination + "</td><td>" + childSnapshot.val().frequency + "</td></tr>");
     $("#tableOfTrains").append(newRowItem);
-    
+
 
 
 
