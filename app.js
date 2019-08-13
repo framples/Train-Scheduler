6ced9b7be4f9ebfc324c7511f67b3772dcbb2043
Illio, the Great <pwfcoding@gmail.com>
2019-08-13 11:23:42 -0500

    added on click even to button and added reset css

--- app.js
+++ app.js
@@ -7,17 +7,33 @@ var firebaseConfig = {
     storageBucket: "project-08032019.appspot.com",
     messagingSenderId: "245568956678",
     appId: "1:245568956678:web:8695ba8afe22aac3"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  var dataRef = firebase.database();
+var dataRef = firebase.database();
 
-  // setting form initial values to empty
+// setting form initial values to empty
 
-  var name = "";
-  var destionation = "";
-  var firstTrainTime = "";
-  var frequency = "";
+var name = "";
+var destination = "";
+var firstTrainTime = "";
+var frequency = "";
 
-  
+
+// on click event to set variables to input on form
+$("#add-train").on("click", function (event) {
+    event.preventDefault();
+
+    name = $("#inputName").val().trim();
+    destination = $("#inputDestination").val().trim();
+    firstTrainTime = $("#inputFirst").val().trim();
+    frequency = $("#inputFrequency").val().trim();
+
+    dataRef.ref().push({
+
+
+    })
+
+
+})
