0e2a7d6bf6b5d73398aaae4dfbd9a5a3da531e1e
Illio, the Great <pwfcoding@gmail.com>
2019-08-13 11:05:18 -0500

    added firebase script links and initial firebase config info to js file

---
+++ app.js
@@ -0,0 +1,14 @@
+// firebase information to connet to server
+var firebaseConfig = {
+    apiKey: "AIzaSyDi-EdArtSeaDWZAEpFNmL4h-WujWK3aNA",
+    authDomain: "project-08032019.firebaseapp.com",
+    databaseURL: "https://project-08032019.firebaseio.com",
+    projectId: "project-08032019",
+    storageBucket: "project-08032019.appspot.com",
+    messagingSenderId: "245568956678",
+    appId: "1:245568956678:web:8695ba8afe22aac3"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var dataRef = firebase.database();
